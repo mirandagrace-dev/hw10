c3ad0e31dd1984b47466476804dd11cd83287c39
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-01-14 21:42:42 -0500

    functionality completed

--- Develop/app.js
+++ Develop/app.js
@@ -9,10 +9,140 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { fileURLToPath } = require("url");
 
+const employees = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+getManager();
+
+function getManager() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "name",
+				message: "Name:",
+			},
+			{
+				type: "input",
+				name: "id",
+				message: "ID:",
+			},
+			{
+				type: "input",
+				name: "email",
+				message: "Email:",
+			},
+			{
+				type: "input",
+				name: "officeNum",
+				message: "Office Number:",
+			},
+		])
+		.then((val) => {
+			const manager = new Manager(val.name, val.id, val.email);
+			employees.push(manager);
+
+			addEmployee();
+		});
+}
+
+function addIntern() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "name",
+				message: "Name:",
+			},
+			{
+				type: "input",
+				name: "id",
+				message: "ID:",
+			},
+			{
+				type: "input",
+				name: "email",
+				message: "Email:",
+			},
+			{
+				type: "input",
+				name: "school",
+				message: "School:",
+			},
+		])
+		.then((val) => {
+			const intern = new Intern(val.name, val.id, val.email, val.school);
+			employees.push(intern);
+
+			addEmployee();
+		});
+}
+
+function addEngineer() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "name",
+				message: "Name: ",
+			},
+			{
+				type: "input",
+				name: "id",
+				message: "ID: ",
+			},
+			{
+				type: "input",
+				name: "email",
+				message: "Email: ",
+			},
+			{
+				type: "input",
+				name: "github",
+				message: "GitHub: ",
+			},
+		])
+		.then((val) => {
+			const engineer = new Engineer(val.name, val.id, val.email, val.github);
+			employees.push(engineer);
+
+			addEmployee();
+		});
+}
+
+function addEmployee() {
+	inquirer
+		.prompt([
+			{
+				type: "list",
+				name: "choice",
+				message: "Do you want to add another employee?",
+				choices: ["Engineer", "Intern", "I'm done."],
+			},
+		])
+		.then((val) => {
+			if (val.choice === "Engineer") {
+				addEngineer();
+			} else if (val.choice === "Intern") {
+				addIntern();
+			} else {
+				generateEmployees();
+			}
+		});
+}
+
+function generateEmployees() {
+	fs.writeFileSync(outputPath, render(employees), (err) => {
+		if (err) {
+			console.log("nope.");
+		} else {
+			console.log("Rendered Successfully!!");
+		}
+	});
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +163,12 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+// get manager function
+//take response and create manager object from that response and then push that object onto a array of employees
+//create another function to ask if there is another employee -- yes/no
+// click yes - go to another method that will ask what kind of employee they want to create
+//
+
+//run the render funtion on the array of employees, store into a variable, then use fs to write to a new html file
+// that variable is the thing
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,33 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+	constructor(name, id, email) {
+		this.name = name;
+		this.id = id;
+		this.email = email;
+	}
+
+	getName() {
+		return this.name;
+	}
+
+	getId() {
+		return this.id;
+	}
+
+	getEmail() {
+		return this.email;
+	}
+
+	getRole() {
+		return "Employee";
+	}
+}
+
+module.exports = Employee;
+
+// needed in each method that writes to a file
+// // fs.appendFile("log.txt", showData, err => {
+//       if (err) throw err;
+
+//       console.log(showData);
+//     });
